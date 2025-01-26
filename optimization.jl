ea92f5d613bf8e39804c006f04bc60e731af478b
Mael HAMET <mael.hamet44@gmail.com>
2025-01-26 17:49:11 +0100

    first optim

--- optimization.jl
+++ optimization.jl
@@ -1,13 +1,36 @@
-using JuMP, CPLEX
+using JuMP, CPLEX, CSV, DataFrames
+
+input_directory = "data/dataset_4/"
+
+X = CSV.read(joinpath(input_directory, "X.csv"), DataFrame, header=false)
+Y = CSV.read(joinpath(input_directory, "Y.csv"), DataFrame, header=false)
+
+N = [k for k in 1:size(X, 2)]   #nb of criterions
+J = [k for k in 1:size(X, 1)]   #nb of samples
+println(N)
 
 
-function create_and_solve_model()
+function create_and_solve_model(X,Y)
 
     #---------Model----------#
     model = Model(CPLEX.Optimizer)
 
     # Decision variables
-    @variable(model, v_ikl[N, K, L])
+    @variable(model, 1>=v_ji[J, N]>=0)  #for j pairs and i criterions
 
+    @variable(model, epsilon[J]>=0)     #for j pairs
+
+    #constraints
+
+    @constraint(model, c1[j in J],
+    sum(v_ji[j, n] * X[j, n] for n in N) - sum(v_ji[j, n] * Y[j, n] for n in N) + epsilon[j] >= 0
+)
+
+
+    @objective(model, Min, sum(epsilon[:]))
+    optimize!(model)
+    println(solution_summary(model))
 
 end
+
+create_and_solve_model(X,Y)
